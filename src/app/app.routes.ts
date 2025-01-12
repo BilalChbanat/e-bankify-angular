cd79b75b9295e34a4bf50021f6f84744c5349526
BilalChbanat <bilal.chbanat2003@gmail.com>
2025-01-12 18:59:51 +0100

    fixint some errors and adding some routs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './auth/register/register.component';
+import {LoginComponent} from './auth/login/login.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
 
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '', component: HomeComponent}
 ];
